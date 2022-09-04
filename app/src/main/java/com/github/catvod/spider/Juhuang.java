6b6da776d470212f9a925c4c0ca0d061169bf1e5
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2022-09-04 21:56:13 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/Juhuang.java
+++ app/src/main/java/com/github/catvod/spider/Juhuang.java
@@ -3,19 +3,10 @@ package com.github.catvod.spider;
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.Base64;
-
 import com.github.catvod.crawler.Spider;
 import com.github.catvod.crawler.SpiderDebug;
+import com.github.catvod.utils.okhttp.OKCallBack;
 import com.github.catvod.utils.okhttp.OkHttpUtil;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -27,64 +18,57 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class Juhuang extends Spider {
-
     private static final String siteUrl = "https://juhuang.tv";
     private static final String siteHost = "juhuang.tv";
 
-    protected JSONObject playerConfig;
-    protected JSONObject filterConfig;
+    /**
+     * 播放源配置
+     */
+    private JSONObject playerConfig;
+    /**
+     * 筛选配置
+     */
+    private JSONObject filterConfig;
 
-    protected Pattern regexCategory = Pattern.compile("/type/(\\d+)_type.html");
-    protected Pattern regexVid = Pattern.compile("/play/(\\d+)_play_\\d+_\\d+.html");
-    protected Pattern regexPlay = Pattern.compile("/play/(\\d+)_play_(\\d+)_(\\d+).html");
-    protected Pattern regexPage = Pattern.compile("/type/\\d+_type_(\\d+).html");
+    private Pattern regexCategory = Pattern.compile("/type/(\\d+)_type.html");
+    private Pattern regexVid = Pattern.compile("/play/(\\d+)_play_\\d+_\\d+.html");
+    private Pattern regexPlay = Pattern.compile("/play/(\\d+)_play_(\\d+)_(\\d+).html");
+    private Pattern regexPage = Pattern.compile("/type/\\d+_type_(\\d+).html");
 
-    @Override
     public void init(Context context) {
-        super.init(context);
+        Juhuang.super.init(context);
         try {
             playerConfig = new JSONObject("{\"xg\":{\"sh\":\"xg播放器\",\"or\":999,\"sn\":0,\"pu\":\"\"},\"dplayer\":{\"sh\":\"dplayer播放器\",\"or\":999,\"sn\":0,\"pu\":\"\"},\"videojs\":{\"sh\":\"videojs-H5播放器\",\"or\":999,\"sn\":0,\"pu\":\"\"},\"iva\":{\"sh\":\"iva-H5播放器\",\"or\":999,\"sn\":0,\"pu\":\"\"},\"iframe\":{\"sh\":\"iframe外链数据\",\"or\":999,\"sn\":0,\"pu\":\"\"},\"link\":{\"sh\":\"外链数据\",\"or\":999,\"sn\":0,\"pu\":\"\"},\"swf\":{\"sh\":\"Flash文件\",\"or\":999,\"sn\":0,\"pu\":\"\"},\"flv\":{\"sh\":\"Flv文件\",\"or\":999,\"sn\":0,\"pu\":\"\"},\"plyr\":{\"sh\":\"plyr\",\"or\":999,\"sn\":0,\"pu\":\"\"},\"H5player\":{\"sh\":\"H5player\",\"or\":999,\"sn\":0,\"pu\":\"\"},\"playerjs\":{\"sh\":\"playerjs\",\"or\":999,\"sn\":0,\"pu\":\"\"},\"aliplayer\":{\"sh\":\"阿里播放器\",\"or\":999,\"sn\":0,\"pu\":\"\"}}");
-
+            filterConfig = new JSONObject("{}");
         } catch (JSONException e) {
             SpiderDebug.log(e);
         }
     }
 
-    protected static HashMap<String, String> Headers() {
-        HashMap<String, String> headers = new HashMap<>();
-        headers.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.62 Safari/537.36");
-        headers.put("Host", "so.juhuang.tv");
-        headers.put("Referer", "https://juhuang.tv/");
-        return headers;
-    }
-
-    /**
-     * 爬虫headers
-     *
-     * @param refererUrl
-     * @return
-     */
-    protected HashMap<String, String> getHeaders(String refererUrl) {
+    protected HashMap<String, String> getHeaders(String url) {
         HashMap<String, String> headers = new HashMap<>();
         headers.put("method", "GET");
-        headers.put("Host", siteHost);
-        headers.put("Referer", refererUrl);
+        headers.put("Host", "juhuang.tv");
+        headers.put("Upgrade-Insecure-Requests", "1");
+        headers.put("DNT", "1");
         headers.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36");
+        headers.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        headers.put("Accept-Language", "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2");
         return headers;
     }
 
-    /**
-     * 获取分类数据 + 首页最近更新视频列表数据
-     *
-     * @param filter 是否开启筛选 关联的是 软件设置中 首页数据源里的筛选开关
-     * @return
-     */
-    @Override
     public String homeContent(boolean filter) {
-        try {
-            String url = siteUrl + '/';
+        try{
             Document doc = Jsoup.parse(OkHttpUtil.string(siteUrl, getHeaders(siteUrl)));
             // 分类节点
             Elements elements = doc.select("ul.nav-menu-items > li > a");
@@ -93,8 +77,8 @@ public class Juhuang extends Spider {
                 //分类名
                 String name = ele.text();
                 boolean show = name.equals("Youtube精选") ||
-                        name.equals("电影") ||
                         name.equals("剧集") ||
+                        name.equals("电影") ||
                         name.equals("综艺") ||
                         name.equals("动漫") ||
                         name.equals("纪录片");
@@ -117,7 +101,8 @@ public class Juhuang extends Spider {
             result.put("class", classes);
             try {
                 // 取首页推荐视频列表
-                Elements list = doc.select("div.module-items>div");
+                Element homeList = doc.select("div[class='module-items']").get(1);
+                Elements list = homeList.select("div.module-item");
                 JSONArray videos = new JSONArray();
                 for (int i = 0; i < list.size(); i++) {
                     Element vod = list.get(i);
@@ -158,18 +143,20 @@ public class Juhuang extends Spider {
     @Override
     public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) {
         try {
-            String url = siteUrl + "/type/" + tid + "_type.html";
-            if (pg != null && Integer.parseInt(pg) > 1) {
-                url = siteUrl + String.format("/type/%s_type_%s.html", tid, pg);
-            }
+            int pageCount = 0;
+            int page = -1;
             // 获取分类数据的url
+            String url = siteUrl + "/type/" + tid + "_type.html";
 
+            if (TextUtils.isEmpty(pg) || Integer.parseInt(pg.trim()) <= 1) {
+                page = 1;
+            } else {
+                url = siteUrl + String.format("/type/%s_type_%s.html", tid, pg);
+                page = Integer.parseInt(pg.trim());
+            }
             String html = OkHttpUtil.string(url, getHeaders(url));
             Document doc = Jsoup.parse(html);
             JSONObject result = new JSONObject();
-            int pageCount = 1;
-            int page = -1;
-//            int page = Integer.parseInt(doc.select("div.module-footer >div[id=page] > span").text().trim());
             // 取页码相关信息
             Elements pageInfo = doc.select("div.module-footer >div[id=page] > a");
             if (pageInfo.size() == 0) {
@@ -177,31 +164,34 @@ public class Juhuang extends Spider {
                 pageCount = page;
             } else {
                 for (int i = 0; i < pageInfo.size(); i++) {
-                    Element div = pageInfo.get(i);
-                    Element a = div.selectFirst("a");
+                    Element li = pageInfo.get(i);
+                    Element a = li.selectFirst("a");
                     if (a == null)
-                        continue;
+                    continue;
                     String name = a.text();
-
                     if (name.equals("尾页")) {
                         Matcher matcher = regexPage.matcher(a.attr("href"));
                         if (matcher.find()) {
                             pageCount = Integer.parseInt(matcher.group(1));
+                        } else {
+                            pageCount = 0;
                         }
                         break;
                     }
                 }
             }
-
             JSONArray videos = new JSONArray();
             if (!html.contains("没有找到您想要的结果哦")) {
-                // 取当前分类页的视频列表
                 Elements list = doc.select("div.module-items>div");
                 for (int i = 0; i < list.size(); i++) {
                     Element vod = list.get(i);
                     String title = vod.selectFirst("div.module-item-cover > div.module-item-pic > a").attr("title");
                     String cover = vod.selectFirst("div.module-item-cover > div.module-item-pic > img").attr("data-src");
-//                    String remark = vod.selectFirst("div.module-item-text").text();
+                    String remark = "";
+                    Element remarkElement = vod.selectFirst("div.module-item-text");
+                    if (remarkElement != null) {
+                        remark = remarkElement.text();
+                    }
                     Matcher matcher = regexVid.matcher(vod.selectFirst("div.module-item-cover > div.module-item-pic > a").attr("href"));
                     if (!matcher.find())
                         continue;
@@ -210,7 +200,7 @@ public class Juhuang extends Spider {
                     v.put("vod_id", id);
                     v.put("vod_name", title);
                     v.put("vod_pic", cover);
-//                    v.put("vod_remarks", remark);
+                    v.put("vod_remarks", remark);
                     videos.put(v);
                 }
             }
@@ -218,7 +208,6 @@ public class Juhuang extends Spider {
             result.put("pagecount", pageCount);
             result.put("limit", 48);
             result.put("total", pageCount <= 1 ? videos.length() : pageCount * 48);
-
             result.put("list", videos);
             return result.toString();
         } catch (Exception e) {
@@ -226,7 +215,6 @@ public class Juhuang extends Spider {
         }
         return "";
     }
-
     /**
      * 视频详情信息
      *
@@ -238,22 +226,25 @@ public class Juhuang extends Spider {
         try {
             // 视频详情url
             String url = siteUrl + "/vod/" + ids.get(0) + "_vod.html";
-            //System.out.println(url);
             Document doc = Jsoup.parse(OkHttpUtil.string(url, getHeaders(url)));
             JSONObject result = new JSONObject();
             JSONObject vodList = new JSONObject();
-
             // 取基本数据
             String cover = doc.selectFirst("div.module-item-pic > img").attr("data-src");
             String title = doc.selectFirst("div.video-info-header > h1.page-title").text();
-            String desc = doc.selectFirst("p.zkjj_a").text();
-
+            String desc = doc.select("p.zkjj_a").text();
+            String category = "", area = "", year = "", remark = "", director = "", actor = "";
 
             vodList.put("vod_id", ids.get(0));
             vodList.put("vod_name", title);
             vodList.put("vod_pic", cover);
+            vodList.put("type_name", category);
+            vodList.put("vod_year", year);
+            vodList.put("vod_area", area);
+            vodList.put("vod_remarks", remark);
+            vodList.put("vod_actor", actor);
+            vodList.put("vod_director", director);
             vodList.put("vod_content", desc);
-            //System.out.println(vodList.toString());
             Map<String, String> vod_play = new TreeMap<>(new Comparator<String>() {
                 @Override
                 public int compare(String o1, String o2) {
@@ -271,31 +262,30 @@ public class Juhuang extends Spider {
                     return 1;
                 }
             });
-
             // 取播放列表数据
-            Elements modules = doc.select("div.module");
+            Elements sources = doc.select("div.module");
+            for (int i = 0; i < sources.size(); i++) {
+                Element source = sources.get(i);
+                String sourceName = source.select("div.module-heading > h2.module-title").text();
 
-
-            for (int i = 0; i < modules.size() - 1; i++) {
-                Element playSourceDoc = modules.get(i);
-                Elements source = playSourceDoc.select("div.module-heading > h2.module-title");
-                String sourceName = source.text();
-                List<String> vodItems = new ArrayList<>();
-                Elements playChapter = playSourceDoc.select("div.module-list > div.module-blocklist > div.scroll-content > a");
-                for (int j = 0; j < playChapter.size(); j++) {
-                    Element aEle = playChapter.get(j);
-                    String chapterHref = aEle.attr("href");
-                    String playUrl = chapterHref.substring(6, chapterHref.indexOf(".html"));
-                    String chapterName = aEle.select("span").text();
-                    vodItems.add(chapterName + "$" + playUrl);
-                }
                 String playList = "";
-                if (vodItems.size() > 0) {
-                    playList = TextUtils.join("#", vodItems);
+                Elements playListA = source.select("div.module-list > div.module-blocklist > div.scroll-content > a");
+                List<String> vodItems = new ArrayList<>();
+
+                for (int j = 0; j < playListA.size(); j++) {
+                    Element vod = playListA.get(j);
+                    String href = vod.attr("href");
+                    String playURL = href.substring(6, href.indexOf(".html"));
+                    vodItems.add(vod.select("span").text() + "$" + playURL);
                 }
+                if (vodItems.size() > 0)
+                    playList = TextUtils.join("#", vodItems);
+
+                if (playList.length() == 0)
+                    continue;
+
                 vod_play.put(sourceName, playList);
             }
-
 
             if (vod_play.size() > 0) {
                 String vod_play_from = TextUtils.join("$$$", vod_play.keySet());
@@ -313,7 +303,6 @@ public class Juhuang extends Spider {
         return "";
     }
 
-
     /**
      * 获取视频播放信息
      *
@@ -325,54 +314,48 @@ public class Juhuang extends Spider {
     @Override
     public String playerContent(String flag, String id, List<String> vipFlags) {
         try {
-            //定义播放用的headers
             JSONObject headers = new JSONObject();
-            headers.put("origin", " https://juhuang.tv");
+            //headers.put("Origin", " https://juhuang.tv");
             headers.put("User-Agent", " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36");
-            headers.put("Accept", " */*");
-            headers.put("Accept-Language", " zh-CN,zh;q=0.9,en-US;q=0.3,en;q=0.7");
-            headers.put("Accept-Encoding", " gzip, deflate");
-            headers.put("Referer", " https://juhuang.tv/");
-
+            //headers.put("Accept", " */*");
+            //headers.put("Accept-Language", " zh-CN,zh;q=0.9,en-US;q=0.3,en;q=0.7");
+            //headers.put("Accept-Encoding", " gzip, deflate");
+            //headers.put("Referer", " https://juhuang.tv/");
 
             // 播放页 url
             String url = siteUrl + "/play/" + id + ".html";
             Document doc = Jsoup.parse(OkHttpUtil.string(url, getHeaders(url)));
             Elements allScript = doc.select("script");
+
             JSONObject result = new JSONObject();
             for (int i = 0; i < allScript.size(); i++) {
                 String scContent = allScript.get(i).html().trim();
-                if (scContent.startsWith("var player_")) { // 取直链
+                if (scContent.startsWith("var player_aaaa")) {
                     int start = scContent.indexOf('{');
                     int end = scContent.lastIndexOf('}') + 1;
                     String json = scContent.substring(start, end);
                     JSONObject player = new JSONObject(json);
                     if (playerConfig.has(player.getString("from"))) {
-                        JSONObject pCfg = playerConfig.getJSONObject(player.getString("from"));
-                        String videoUrl = player.getString("url");
-
+                        String jxurl = player.getString("url");
                         if (player.has("encrypt")) {
                             int encrypt = player.getInt("encrypt");
                             if (encrypt == 1) {
-                                videoUrl = URLDecoder.decode(videoUrl);
+                                jxurl = URLDecoder.decode(jxurl);
                             } else if (encrypt == 2) {
-                                videoUrl = new String(Base64.decode(videoUrl, Base64.DEFAULT));
-                                videoUrl = URLEncoder.encode(new String(Base64.decode(URLDecoder.decode(videoUrl),Base64.DEFAULT)));
+                                jxurl = URLEncoder.encode(new String(Base64.decode(URLDecoder.decode(new String(Base64.decode(jxurl, 0))),0)));
                             }
                         }
-                        String jxvideoUrl = "https://web-webapi-tsjqsvyzyx.cn-shenzhen.fcapp.run/?url=" + videoUrl;
-                        Map<String, String> jxheader = new HashMap<>();
-                        jxheader.put("Referer", " https://juhuang.tv/");
-                        jxheader.put("User-Agent", " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36");
-                        JSONObject Urljson = new JSONObject(OkHttpUtil.string(jxvideoUrl, jxheader));
-                        String RealUrl = Urljson.getString("play_url");
-//                        String FinalUrl = "https://" + RealUrl;
-                        result.put("url", RealUrl);
+                        String directUrl = "https://web-webapi-tsjqsvyzyx.cn-shenzhen.fcapp.run/?url=" + jxurl;
+                        HashMap hashMap = new HashMap();
+                        hashMap.put("Referer", " https://juhuang.tv/");
+                        hashMap.put("Origin", " https://juhuang.tv");
+                        hashMap.put("User-Agent", " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36");
+                        result.put("url", new JSONObject(OkHttpUtil.string(directUrl, hashMap)).getString("play_url"));
                         result.put("parse", "0");
                         result.put("playUrl", "");
-                        result.put("header", "");
+                        result.put("header", headers.toString());
+                        //result.put("header", "{\"User-Agent\", \" Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36\"}");
                     }
-                    break;
                 }
             }
             return result.toString();
@@ -382,14 +365,20 @@ public class Juhuang extends Spider {
         return "";
     }
 
-
+    /**
+     * 搜索
+     *
+     * @param key
+     * @param quick 是否播放页的快捷搜索
+     * @return
+     */
     @Override
     public String searchContent(String key, boolean quick) {
         try {
             if (quick)
                 return "";
-            String url = "https://so.juhuang.tv/soapi.php?wd=" + URLEncoder.encode(key);
-            JSONObject searchResult = new JSONObject(OkHttpUtil.string(url, Headers()));
+            String url = "https://so.juhuang.tv/soapi.php?&wd=" + URLEncoder.encode(key);
+            JSONObject searchResult = new JSONObject(OkHttpUtil.string(url, getHeaders(url)));
             JSONObject result = new JSONObject();
             JSONArray videos = new JSONArray();
             if (searchResult.getInt("count") > 0) {
